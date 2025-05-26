f3bc2860d64542db8cdc681ac9b4fc20aa16c436
Ivan <ikuznets93@gmail.com>
2025-05-26 21:10:24 +0300

    rename and move common.py

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,21 +1,21 @@
-import brain_games.common as common
+import brain_games.game_engine.engine as engine
 from brain_games.games.gcd import gcd_game
 
 
 def main():
-    rules = 'Find the greatest common divisor of given numbers.'
+    rules = 'Find the greatest engine divisor of given numbers.'
     num_of_games = 3
         
-    name = common.welcome_user(rules)
+    name = engine.welcome_user(rules)
     for _ in range(num_of_games):
         question, answer = gcd_game()
-        common.ask_question(question)
-        user_answer = common.get_user_answer()
-        is_correct_answer = common.is_correct(answer, user_answer)
-        common.correctness_message(answer, user_answer, is_correct_answer)
+        engine.ask_question(question)
+        user_answer = engine.get_user_answer()
+        is_correct_answer = engine.is_correct(answer, user_answer)
+        engine.correctness_message(answer, user_answer, is_correct_answer)
         if is_correct_answer is False:
-            return common.failure_message(name)
-    common.congrats_message(name)
+            return engine.failure_message(name)
+    engine.congrats_message(name)
 
 
 if __name__ == "__main__":
